a5f63b1667f01beb0628686cdfb6e6798b67bc7d
Jordi Enric <jordi.err@gmail.com>
2024-04-21 15:28:22 +0200

    rm folder idk anymore?

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,4 +1,6 @@
-import "./globals.css";
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
+import { FaUserAstronaut } from "react-icons/fa";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -10,15 +12,44 @@ export const metadata = {
   description: "The fastest way to add subscriptions to your app",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const supa = createClient();
+  const user = await supa.auth.getUser();
+
   return (
     <html lang="en" className={""}>
       <body className="">
-        <main className="min-h-screen max-w-2xl mx-auto">{children}</main>
+        <div className="bg-white">
+          <header className="max-w-2xl mx-auto flex items-center justify-between px-4 py-2">
+            <Link
+              href="/projects"
+              className="font-bold tracking-tight flex items-center text-zinc-800"
+            >
+              <span className="text-2xl mr-1">✌️</span>
+              <span>Zenpay</span>
+              <span className="font-mono text-xs mx-2 text-orange-500">
+                BETA
+              </span>
+            </Link>
+            <nav>
+              <Link
+                href="/projects"
+                className="flex text-sm tracking-tight text-zinc-800 p-2 border rounded-full hover:bg-white transition-all"
+              >
+                <FaUserAstronaut size="18" />
+              </Link>
+            </nav>
+          </header>
+        </div>
+
+        <main className="">{children}</main>
+        <footer className="text-center py-12 text-sm text-zinc-600 font-mono tracking-tighter">
+          Zenpay Beta
+        </footer>
       </body>
     </html>
   );
